36ad14cbd3008be6e071910984bf799c5b86ebc0
Dat Vu <redb1997@gmail.com>
2020-03-30 21:14:58 +0300

    initial commit

---
+++ src/utils/customHooks/cutstomHooks.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useReducer, useRef } from "react";
+import isEqual from "lodash/isEqual";
+
+export function useSetState(initialState) {
+  const [state, newState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    initialState
+  );
+  return [state, newState];
+}
+
+export function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+export function useDeepCompareEffect(callback, inputs) {
+  const cleanupRef = useRef();
+  useEffect(() => {
+    if (!isEqual(previousInputs, inputs)) {
+      cleanupRef.current = callback();
+    }
+    return cleanupRef.current;
+  });
+
+  const previousInputs = usePrevious(inputs);
+}
